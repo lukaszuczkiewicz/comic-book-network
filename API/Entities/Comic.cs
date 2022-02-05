39b65ce89926d7756c2bba32015eeed837b93a22
Lukasz <lukaszuczkiewicz@gmail.com>
2022-02-06 00:06:45 +0100

    api: add new publish dates and new comics

--- API/Entities/Comic.cs
+++ API/Entities/Comic.cs
@@ -1,8 +1,12 @@
-﻿namespace API.Entities
+﻿using API.Extentions;
+using System.Text.Json.Serialization;
+
+namespace API.Entities
 {
     public class Comic
     {
         public int Id { get; set; }
+        [property: JsonConverter(typeof(DateOnlyConverter))]
         public DateOnly PublishDate { get; set; }
         public int IssueNumber { get; set; }
         public decimal Price { get; set; }

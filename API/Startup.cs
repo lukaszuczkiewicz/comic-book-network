87f366e1406abd6694797786a81d0c538b553979
Lukasz <lukaszuczkiewicz@gmail.com>
2022-02-19 22:27:51 +0100

    enable static fe

--- API/Startup.cs
+++ API/Startup.cs
@@ -66,9 +66,13 @@ namespace API
             app.UseAuthentication();
             app.UseAuthorization();
 
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+                endpoints.MapFallbackToController("Index", "Fallback");
             });
         }
     }
